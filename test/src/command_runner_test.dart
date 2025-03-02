c35ef13245bc2742248934975c9be51b36eeba43
Szymon Kowaliński <szymon@kowalinski.dev>
2025-03-02 18:11:07 +0100

    test: remove bad test

--- test/src/command_runner_test.dart
+++ test/src/command_runner_test.dart
@@ -30,22 +30,15 @@ void main() {
     setUp(() {
       pubUpdater = _MockPubUpdater();
 
-      when(
-        () => pubUpdater.getLatestVersion(any()),
-      ).thenAnswer((_) async => packageVersion);
+      when(() => pubUpdater.getLatestVersion(any())).thenAnswer((_) async => packageVersion);
 
       logger = _MockLogger();
 
-      commandRunner = SolvroConfigCommandRunner(
-        logger: logger,
-        pubUpdater: pubUpdater,
-      );
+      commandRunner = SolvroConfigCommandRunner(logger: logger, pubUpdater: pubUpdater);
     });
 
     test("shows update message when newer version exists", () async {
-      when(
-        () => pubUpdater.getLatestVersion(any()),
-      ).thenAnswer((_) async => latestVersion);
+      when(() => pubUpdater.getLatestVersion(any())).thenAnswer((_) async => latestVersion);
 
       final result = await commandRunner.run(["--version"]);
       expect(result, equals(ExitCode.success.code));
@@ -54,9 +47,7 @@ void main() {
 
     test("Does not show update message when the shell calls the "
         "completion command", () async {
-      when(
-        () => pubUpdater.getLatestVersion(any()),
-      ).thenAnswer((_) async => latestVersion);
+      when(() => pubUpdater.getLatestVersion(any())).thenAnswer((_) async => latestVersion);
 
       final result = await commandRunner.run(["completion"]);
       expect(result, equals(ExitCode.success.code));
@@ -64,22 +55,13 @@ void main() {
     });
 
     test("does not show update message when using update command", () async {
+      when(() => pubUpdater.getLatestVersion(any())).thenAnswer((_) async => latestVersion);
       when(
-        () => pubUpdater.getLatestVersion(any()),
-      ).thenAnswer((_) async => latestVersion);
+        () => pubUpdater.update(packageName: packageName, versionConstraint: any(named: "versionConstraint")),
+      ).thenAnswer((_) async => ProcessResult(0, ExitCode.success.code, null, null));
       when(
-        () => pubUpdater.update(
-          packageName: packageName,
-          versionConstraint: any(named: "versionConstraint"),
-        ),
-      ).thenAnswer(
-        (_) async => ProcessResult(0, ExitCode.success.code, null, null),
-      );
-      when(
-        () => pubUpdater.isUpToDate(
-          packageName: any(named: "packageName"),
-          currentVersion: any(named: "currentVersion"),
-        ),
+        () =>
+            pubUpdater.isUpToDate(packageName: any(named: "packageName"), currentVersion: any(named: "currentVersion")),
       ).thenAnswer((_) async => true);
 
       final progress = _MockProgress();
@@ -95,14 +77,11 @@ void main() {
       verifyNever(() => logger.info(updatePrompt));
     });
 
-    test(
-      "can be instantiated without an explicit analytics/logger instance",
-      () {
-        final commandRunner = SolvroConfigCommandRunner();
-        expect(commandRunner, isNotNull);
-        expect(commandRunner, isA<CompletionCommandRunner<int>>());
-      },
-    );
+    test("can be instantiated without an explicit analytics/logger instance", () {
+      final commandRunner = SolvroConfigCommandRunner();
+      expect(commandRunner, isNotNull);
+      expect(commandRunner, isA<CompletionCommandRunner<int>>());
+    });
 
     test("handles FormatException", () async {
       const exception = FormatException("oops!");
@@ -151,22 +130,6 @@ void main() {
         verify(() => logger.detail("  Top level options:")).called(1);
         verify(() => logger.detail("  - verbose: true")).called(1);
         verifyNever(() => logger.detail("    Command options:"));
-      });
-
-      test("enables verbose logging for sub commands", () async {
-        final result = await commandRunner.run([
-          "--verbose",
-          "install",
-          "--package",
-        ]);
-        expect(result, equals(ExitCode.success.code));
-
-        verify(() => logger.detail("Argument information:")).called(1);
-        verify(() => logger.detail("  Top level options:")).called(1);
-        verify(() => logger.detail("  - verbose: true")).called(1);
-        verify(() => logger.detail("  Command: install")).called(1);
-        verify(() => logger.detail("    Command options:")).called(1);
-        verify(() => logger.detail("    - package: true")).called(1);
       });
     });
   });
